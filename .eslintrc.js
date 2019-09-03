ef0619671b349baae52c851b8426502ac157ffce
Michael Villander <nousvillander@gmail.com>
2019-09-03 20:48:46 -0300

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,7 +43,8 @@ module.exports = {
 
         // this can be removed once the following is fixed
         // https://github.com/mysticatea/eslint-plugin-node/issues/77
-        'node/no-unpublished-require': 'off'
+        'node/no-unpublished-require': 'off',
+        'node/no-extraneous-require': 'off'
       })
     }
   ]
