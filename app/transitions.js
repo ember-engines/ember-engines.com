7e2240596659a1700377eb0bec26d2c8ecb0fce5
Michael Villander <nousvillander@gmail.com>
2019-09-03 20:33:18 -0300

    upgrade ember-cli & ember-engines

--- app/transitions.js
+++ app/transitions.js
@@ -1,9 +1,7 @@
 import Ember from 'ember';
 
-const DURATION = Ember.testing ? 0 : 300;
-
 export default function transitionMap() {
-  this.setDefault({ duration: DURATION });
+  this.setDefault({ duration: Ember.testing ? 0 : 300 });
 
   this.transition(
     this.fromRoute('index'),
