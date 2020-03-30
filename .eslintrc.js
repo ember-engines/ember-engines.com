a81f78417f2d3b754a8e5e508257dd3ac52bd539
Gabriel Csapo <gabecsapo@gmail.com>
2020-03-29 17:18:56 -0700

    reverts ember update which causes ui issues, remove tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,8 @@
-'use strict';
-
 module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
-    ecmaFeatures: {
-      legacyDecorators: true
-    }
+    sourceType: 'module'
   },
   plugins: [
     'ember'
